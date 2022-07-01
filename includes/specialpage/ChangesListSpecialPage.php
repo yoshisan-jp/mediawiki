6feb3bf809d17f01840b654277287ef1890d4563
Zabe <zabe@avorwerk.net>
2022-07-01 13:05:18 +0000

    Revert "Revert "RecentChange: Straight join to actor table when needed""
    This reverts commit 488b905874ed20e7b3c352585188e02db8ef026d.
    
    Reason for revert: f24eb9457118 should fix the problem
    
    Bug: T311360
    Change-Id: Ib923b19ac899de7784460e09c16166a66df25e86

--- includes/specialpage/ChangesListSpecialPage.php
+++ includes/specialpage/ChangesListSpecialPage.php
@@ -106,6 +106,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 							IDatabase $dbr, &$tables, &$fields, &$conds, &$query_options, &$join_conds
 						) {
 							$conds['actor_user'] = null;
+							$join_conds['recentchanges_actor'] = [ 'JOIN', 'actor_id=rc_actor' ];
 						},
 						'isReplacedInStructuredUi' => true,
 
@@ -120,6 +121,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 							IDatabase $dbr, &$tables, &$fields, &$conds, &$query_options, &$join_conds
 						) {
 							$conds[] = 'actor_user IS NOT NULL';
+							$join_conds['recentchanges_actor'] = [ 'JOIN', 'actor_id=rc_actor' ];
 						},
 						'isReplacedInStructuredUi' => true,
 					]
@@ -215,6 +217,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 						) {
 							$user = $ctx->getUser();
 							$conds[] = 'actor_name<>' . $dbr->addQuotes( $user->getName() );
+							$join_conds['recentchanges_actor'] = [ 'JOIN', 'actor_id=rc_actor' ];
 						},
 						'cssClassSuffix' => 'self',
 						'isRowApplicableCallable' => static function ( IContextSource $ctx, RecentChange $rc ) {
@@ -235,6 +238,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 							} else {
 								$conds['actor_user'] = $user->getId();
 							}
+							$join_conds['recentchanges_actor'] = [ 'JOIN', 'actor_id=rc_actor' ];
 						},
 						'cssClassSuffix' => 'others',
 						'isRowApplicableCallable' => static function ( IContextSource $ctx, RecentChange $rc ) {
@@ -1759,11 +1763,13 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 			!in_array( 'unregistered', $selectedExpLevels )
 		) {
 			$conds[] = 'actor_user IS NOT NULL';
+			$join_conds['recentchanges_actor'] = [ 'JOIN', 'actor_id=rc_actor' ];
 			return;
 		}
 
 		if ( $selectedExpLevels === [ 'unregistered' ] ) {
 			$conds['actor_user'] = null;
+			$join_conds['recentchanges_actor'] = [ 'JOIN', 'actor_id=rc_actor' ];
 			return;
 		}
 
@@ -1808,6 +1814,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 		if ( in_array( 'unregistered', $selectedExpLevels ) ) {
 			$selectedExpLevels = array_diff( $selectedExpLevels, [ 'unregistered' ] );
 			$conditions['actor_user'] = null;
+			$join_conds['recentchanges_actor'] = [ 'JOIN', 'actor_id=rc_actor' ];
 		}
 
 		if ( $selectedExpLevels === [ 'newcomer' ] ) {
@@ -1830,6 +1837,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 			$conditions[] = $aboveNewcomer;
 		} elseif ( $selectedExpLevels === [ 'experienced', 'learner', 'newcomer' ] ) {
 			$conditions[] = 'actor_user IS NOT NULL';
+			$join_conds['recentchanges_actor'] = [ 'JOIN', 'actor_id=rc_actor' ];
 		}
 
 		if ( count( $conditions ) > 1 ) {
