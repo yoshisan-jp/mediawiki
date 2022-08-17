755236516cd390eaf1347768847ea55e7d38b69a
Ladsgroup <Ladsgroup@gmail.com>
2022-08-17 07:05:16 +0000

    Revert "SpecialRecentChangesLinked: Use rdbms code for building the main query"
    This reverts commit c3100a0811c1f89be23efc3dd059b36d6520ebed.
    
    Reason for revert: Not deploying it now.
    
    Change-Id: I2f73bfbc59a3f0d5ae6878a23b351b69f5fa718c

--- includes/specials/SpecialRecentChangesLinked.php
+++ includes/specials/SpecialRecentChangesLinked.php
@@ -21,11 +21,8 @@
  * @ingroup SpecialPage
  */
 
-use MediaWiki\MainConfigNames;
 use MediaWiki\User\UserOptionsLookup;
 use Wikimedia\Rdbms\ILoadBalancer;
-use Wikimedia\Rdbms\SelectQueryBuilder;
-use Wikimedia\Rdbms\Subquery;
 
 /**
  * This is to display changes made to all articles linked in an article.
@@ -275,16 +272,10 @@ class SpecialRecentChangesLinked extends SpecialRecentChanges {
 		if ( count( $subsql ) == 1 && $dbr->unionSupportsOrderAndLimit() ) {
 			$sql = $subsql[0];
 		} else {
-			$queryBuilder = $dbr->newSelectQueryBuilder()
-				->select( '*' )
-				->from(
-					(string)( new Subquery( $dbr->unionQueries( $subsql, $dbr::UNION_DISTINCT ) ) ),
-					'main'
-				)
-				->orderBy( 'rc_timestamp', SelectQueryBuilder::SORT_DESC )
-				->setMaxExecutionTime( $this->getConfig()->get( MainConfigNames::MaxExecutionTimeForExpensiveQueries ) )
-				->limit( $limit );
-			$sql = $queryBuilder->getSQL();
+			// need to resort and relimit after union
+			$sql = $dbr->unionQueries( $subsql, $dbr::UNION_DISTINCT ) .
+				' ORDER BY rc_timestamp DESC';
+			$sql = $dbr->limitResult( $sql, $limit, false );
 		}
 		return $dbr->query( $sql, __METHOD__ );
 	}
