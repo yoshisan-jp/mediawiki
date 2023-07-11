0de1c4f2c51a5eb063d793ca72105c0409e5eec0
Ladsgroup <Ladsgroup@gmail.com>
2023-07-11 10:45:06 +0000

    Revert "ExternalLinks: Make oneWildcard avoid adding wildcard to domain"
    This reverts commit 198bbf317bcb15de85319745e14eef22ab689880.
    
    Reason for revert: Has a bug
    
    Change-Id: I4318bc2e302b192fae0bb624fc39d6e7e091614c

--- includes/ExternalLinks/LinkFilter.php
+++ includes/ExternalLinks/LinkFilter.php
@@ -367,12 +367,7 @@ class LinkFilter {
 				array_pop( $trimmedlikeDomain );
 			}
 			$index1 = implode( '', $trimmedlikeDomain );
-			if ( $options['oneWildcard'] ) {
-				$domainConditions[] = 'el_to_domain_index = ' . $db->addQuotes( $index1 );
-			} else {
-				$domainConditions[] = "el_to_domain_index" . $db->buildLike( $index1, $db->anyString() );
-			}
-
+			$domainConditions[] = "el_to_domain_index" . $db->buildLike( $index1, $db->anyString() );
 		}
 		if ( !$domainConditions ) {
 			return false;
